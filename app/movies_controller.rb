3e2d8345428819430b515e8b30783d79cc319755
Marc Poppy <poppy.marc@gmail.com>
2020-01-11 20:45:11 +0000

    Automatically backed up by Learn

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -82,8 +82,9 @@ end
 def can_update_using_update_method
   # Update movie title to "Wat, huh?"
   Movie.create(title: "Wat?")
-  __
-  __
+  movie = Movie.find_by(title: "Wat?")
+  movie.title = "Wat, huh?"
+  movie
 end
 
 def can_update_multiple_items_at_once
