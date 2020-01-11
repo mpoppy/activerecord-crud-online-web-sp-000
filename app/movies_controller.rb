7ada9efd2108acf224414f9060e4ef01785ffb1b
Marc Poppy <poppy.marc@gmail.com>
2020-01-11 20:49:14 +0000

    Automatically backed up by Learn

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -77,6 +77,7 @@ def can_be_found_updated_and_saved
   movie.find_by(title: "Awesome Flick")
   movie.update(title: "Even Awesomer Flick")
   movie.save
+  movie
 end
 
 def can_update_using_update_method
